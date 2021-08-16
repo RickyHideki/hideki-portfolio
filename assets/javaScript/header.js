91c709bad700c8dd00299e5e6cf59bd1d2a16764
Ricky Hideki <87090060+RickyHideki@users.noreply.github.com>
2021-08-16 14:07:17 -0300

    Add files via upload

---
+++ assets/javaScript/header.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("header-sideNav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("header-sideNav").style.width = "0";
+} 
